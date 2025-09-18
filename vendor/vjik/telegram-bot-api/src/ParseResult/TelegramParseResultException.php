4687a8d377d6421ab783c1beb2eead49f3ecf83c
Vladislav <mel@mel.rr>
2025-09-18 10:43:08 +0300

    Remove vendor/ from repository

--- vendor/vjik/telegram-bot-api/src/ParseResult/TelegramParseResultException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace Vjik\TelegramBot\Api\ParseResult;
-
-use Vjik\TelegramBot\Api\TelegramRuntimeException;
-
-class TelegramParseResultException extends TelegramRuntimeException {}
